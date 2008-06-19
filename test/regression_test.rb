2566858599b8e6d0994c64631c6849bb2a991674
Bin Dong <you@example.com>
2008-06-19 18:35:53 +0800

    more features in xml-simple added

--- test/regression_test.rb
+++ test/regression_test.rb
@@ -6,19 +6,19 @@ class RegressionTest < FasterXSTest
     assert_equal expected, FasterXmlSimple.xml_in("<asdf><jklsemicolon /></asdf>")
     assert_equal expected, FasterXmlSimple.xml_in("<asdf><jklsemicolon /></asdf>", 'forcearray'=>['asdf'])
   end
-  
+
   def test_s3_regression
     str = File.read("test/fixtures/test-7.xml")
     assert_nil FasterXmlSimple.xml_in(str)["AccessControlPolicy"]["AccessControlList"]["__content__"]
   end
-  
+
   def test_xml_simple_transparency
     assert_equal XmlSimple.xml_in("<asdf />"), FasterXmlSimple.xml_in("<asdf />")
   end
-  
+
   def test_suppress_empty_variations
     str = "<asdf><fdsa /></asdf>"
-    
+
     assert_equal Hash.new, FasterXmlSimple.xml_in(str)["asdf"]["fdsa"]
     assert_nil FasterXmlSimple.xml_in(str, 'suppressempty'=>nil)["asdf"]["fdsa"]
     assert_equal '', FasterXmlSimple.xml_in(str, 'suppressempty'=>'')["asdf"]["fdsa"]
@@ -27,21 +27,47 @@ class RegressionTest < FasterXSTest
 
   def test_empty_string_doesnt_crash
     assert_raise(XML::Parser::ParseError) do
-      silence_stderr do 
+      silence_stderr do
         FasterXmlSimple.xml_in('')
       end
     end
   end
-  
+
   def test_keeproot_false
     str = "<asdf><fdsa>1</fdsa></asdf>"
     expected = {"fdsa"=>"1"}
     assert_equal expected, FasterXmlSimple.xml_in(str, 'keeproot'=>false)
   end
-  
+
   def test_keeproot_false_with_force_content
     str = "<asdf><fdsa>1</fdsa></asdf>"
     expected = {"fdsa"=>{"__content__"=>"1"}}
     assert_equal expected, FasterXmlSimple.xml_in(str, 'keeproot'=>false, 'forcecontent'=>true)
+  end
+
+  def test_group_tag
+    str = "<a>
+            <c>
+            <b>1</b>
+            <b>2</b>
+            </c>
+          </a>"
+    expected = { "a" => { 'c' => ["1", "2"] }}
+    assert_equal expected, FasterXmlSimple.xml_in(str, 'grouptags' => { 'c' => 'b'})
+
+  end
+
+  def test_compress_whitespace
+    str = "<a><b>   a    b   </b></a>"
+    expected = { "a" => { 'b' => " a b "}}
+    assert_equal expected, FasterXmlSimple.xml_in(str, 'compress_whitespace' => ['b'])
+
+  end
+
+  def test_cdata
+    str = "<a> <b><![CDATA[ a ]]></b> </a>"
+    expected = {  "a" => { "b" => " a "}}
+    assert_equal expected, FasterXmlSimple.xml_in(str)
+
   end
 end
